2db75bcb18f87efc4eb040d97497d4d3511b0572
HangNT169 <HangNT169@fpt.edu.vn>
2024-03-29 01:14:05 +0700

    Init

---
+++ DemoNUnit/DemoNUnit/StudentAverage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNUnit
+{
+    public class StudentAverage
+    {
+        public string getAverageRank(double averageScore)
+        {
+            if(averageScore < 0 || averageScore > 10)
+            {
+                throw new ArgumentException("Diem trung binh khong hop le, diem chi trong pham vi tu 0-10");
+            }
+            if(averageScore >= 9)
+            {
+                return "Xuất sắc";
+            }else if(averageScore >= 8)
+            {
+                return "Giỏi";
+            }
+            else if (averageScore >= 7)
+            {
+                return "Khá";
+            }
+            else if (averageScore >= 5)
+            {
+                return "Trung bình";
+            }
+            else
+            {
+                return "Yếu";
+            }
+        }
+    }
+}
---
+++ DemoNUnit/StudentAverageTest/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using DemoNUnit;
+
+namespace StudentAverageTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        StudentAverage studentAverage;
+        [SetUp]
+        public void Setup()
+        {
+            studentAverage = new StudentAverage();
+        }
+
+        [Test]//Check diemTB = -1  -> nem ngoai le
+        public void Test1()
+        {
+            Assert.Throws<ArgumentException>(() => studentAverage.getAverageRank(-1));
+            //Assert.Throws<ArgumentException>(() => studentAverage.getAverageRank(11));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Throws<ArgumentException>(() => studentAverage.getAverageRank(11));
+        }
+        [Test]
+        public void Test3()
+        {
+            //Xuat sac
+            double averageScore = 9.2;
+            //Act
+            string result = studentAverage.getAverageRank(averageScore);
+            Assert.That(result, Is.EqualTo("Xuất sắc"));
+
+        }
+        [Test]
+        public void Test4()
+        {
+            //Xuat sac
+            double averageScore = 8.5;
+            //Act
+            string result = studentAverage.getAverageRank(averageScore);
+            Assert.That(result, Is.EqualTo("Giỏi"));
+
+        }
+        [Test]
+        public void Test5()
+        {
+            //Xuat sac
+            double averageScore = 7.5;
+            //Act
+            string result = studentAverage.getAverageRank(averageScore);
+            Assert.That(result, Is.EqualTo("Khá"));
+
+        }
+        [Test]
+        public void Test6()
+        {
+            //Xuat sac
+            double averageScore = 6.5;
+            //Act
+            string result = studentAverage.getAverageRank(averageScore);
+            Assert.That(result, Is.EqualTo("Trung bình"));
+
+        }
+        [Test]
+        public void Test7()
+        {
+            //Xuat sac
+            double averageScore = 4;
+            //Act
+            string result = studentAverage.getAverageRank(averageScore);
+            Assert.That(result, Is.EqualTo("Yếu"));
+        }
+
+    }
+}
